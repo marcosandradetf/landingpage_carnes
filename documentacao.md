271e8201e70dfd4d7ad045d406a9c7b44fe562dd
Marcos Túlio <marcostfandrade@proton.me>
2023-05-18 01:13:21 -0300

    Add files via upload

---
+++ documentacao.md
@@ -0,0 +1,26 @@
+# Meu projeto
+
+Esta é uma descrição do meu projeto. É um redesign de website que está com diversos problemas, tanto interface quanto na organização.
+
+## Os Problemas que irei solucionar
+
+Para este projeto, mudarei os seguintes pontos:
+
+1. Será criado um header com links para navegação na página e uma logo.
+2. Será criado um menu para visualização mobile tanto deste menu, quanto da página em si.
+3. Melhor organização do conteúdo.
+4. Deixar a aparência mais amigável.
+
+## Tecnologias Utilizadas
+
+Este projeto foi construído utilizando as seguintes tecnologias:
+
+- Html
+- Css
+- JavaScript
+
+## Fontes e cores
+
+- fonts: Squada One and/or Share and/or Open Sans;
+- background-color: red and/or black and/or white;
+- font-colors: red and black.
---
+++ script.js
@@ -0,0 +1,119 @@
+const body = document.body;
+const chat = document.createElement('img')
+chat.src = 'chat.svg';
+chat.style.cssText = 
+  'position: absolute;'+
+  'position: fixed;'+
+  'bottom: 20px;'+
+  'right: 20px;';
+
+
+const header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+  const scrolled = window.scrollY;
+  if (scrolled > 50 && window.screen.width > 768) {
+    header.style.backgroundColor = '#222222';
+    body.appendChild(chat);
+  } else {
+    header.style = '';
+    body.removeChild(chat);
+  }
+});
+
+
+
+var multipleCardCarousel = document.querySelector(
+  "#carouselExampleControls"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false,
+  });
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+  var scrollPosition = 0;
+  $("#carouselExampleControls .carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      scrollPosition += cardWidth;
+      $("#carouselExampleControls .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+  $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#carouselExampleControls .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+} else {
+  $(multipleCardCarousel).addClass("slide");
+}
+
+var multipleCardCarousel2 = document.querySelector(
+  "#carouselExampleControls2"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var carousel = new bootstrap.Carousel(multipleCardCarousel2, {
+    interval: false,
+  });
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+  var scrollPosition = 0;
+  $("#carouselExampleControls2 .carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      scrollPosition += cardWidth;
+      $("#carouselExampleControls2 .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+  $("#carouselExampleControls2 .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#carouselExampleControls2 .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+} else {
+  $(multipleCardCarousel2).addClass("slide");
+}
+
+var multipleCardCarousel3 = document.querySelector(
+  "#carouselExampleControls3"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var carousel = new bootstrap.Carousel(multipleCardCarousel3, {
+    interval: false,
+  });
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+  var scrollPosition = 0;
+  $("#carouselExampleControls3 .carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      scrollPosition += cardWidth;
+      $("#carouselExampleControls3 .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+  $("#carouselExampleControls3 .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#carouselExampleControls3 .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+} else {
+  $(multipleCardCarousel3).addClass("slide");
+}
